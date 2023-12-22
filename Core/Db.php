c3eef9483d0a62693e16978770194cd4e0ff3ba9
dapeeche <91271211+dapeeche@users.noreply.github.com>
2023-12-22 19:23:12 +0200

    add use PDO

--- Core/Db.php
+++ Core/Db.php
@@ -1,23 +1,24 @@
 <?php
 
 namespace Core;
+use PDO;
 
 class Db
 {
-    static protected \PDO|null $instance = null;
+    static protected PDO|null $instance = null;
 
 
 
-    static public function connect(): \PDO
+    static public function connect(): PDO
     {
         if (is_null(static::$instance)) {
             $dsn = "mysql:host=mysql_db;dbname=mvc_db";
             $options = [
-                \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
-                \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
             ];
 
-            return static::$instance = new \PDO(
+            return static::$instance = new PDO(
                 $dsn,
                 'root',
                 'secret',
